399b3d81d758f3d64390dc6687ae251606c414e5
Steve Jung <sjung@coinplug.com>
2018-07-08 12:34:29 -0700

    updated solc.sh to handle libraries

--- misc/solc.sh
+++ misc/solc.sh
@@ -3,15 +3,15 @@
 
 function usage ()
 {
-    echo "$(basename $0) <sol-file>"
+    echo "$(basename $0) [-l <name>:<addr>]+ <sol-file> <js-file>
+
+SOL_LIBS environment variable can be used instead of -l option."
 }
 
-if [ $# != 1 ]; then
-    usage
-    exit 1
-fi
-
-docker run -v $(pwd):/tmp --workdir /tmp --rm ethereum/solc:stable --optimize --abi --bin $* | awk '
+# int compile(string solFile, string jsFile)
+function compile ()
+{
+    docker run -v $(pwd):/tmp --workdir /tmp --rm ethereum/solc:stable --optimize --abi --bin $1 | awk -v libs="$SOL_LIBS" '
 function flush() {
   if (length(code_name) > 0) {
     printf "\
@@ -20,7 +20,7 @@ function %s_new() {\
   {\
     from: web3.eth.accounts[0],\
     data: %s_data,\
-    gas: \"0x2000000\"\
+    gas: \"0x20000000\"\
   }, function (e, contract) {\
     console.log(e, contract);\
     if (typeof contract.address !== \"undefined\") {\
@@ -59,12 +59,46 @@ END {
   }
 }
 
-# binary: 60.06040 for contracts, 610eb861 for libraries
-/^610|^60.06040/ {
+# binary: 60606040, 60806040 for contracts, 610eb861 for libraries
+/^6[01]/ {
   if (length(code_name) > 0) {
-    print "var " code_name "_data = \"0x" $0 "\";";
+    code = $0;
+    n = split(libs, alibs, " +");
+    for (i = 1; i <= n; i++) {
+      if (split(alibs[i], nv, ":") != 2)
+        continue;
+      sub("^0x", "", nv[2]);
+      gsub("_+[^_]*" nv[1] "_+", nv[2], code);
+    }
+    print "var " code_name "_data = \"0x" code "\";";
   }
 }
-';
+' > $2;
+}
+
+args=`getopt l: $*`
+if [ $? != 0 ]; then
+    usage;
+    exit 1;
+fi
+set -- $args
+
+#SOL_LIBS
+for i; do
+    case "$i" in
+    -l)
+	[ "$SOL_LIBS" = "" ] || SOL_LIBS="$SOL_LIBS "
+	SOL_LIBS="${SOL_LIBS}$2";
+	shift;
+	shift;;
+    esac
+done
+
+if [ $# != 3 ]; then
+    usage
+    exit 1
+fi
+
+compile "$2" "$3"
 
 # EOF
