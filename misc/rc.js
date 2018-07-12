30a06d4d86ee8493d5ae7681726158c6758db515
San Kim <kim503@usc.edu>
2018-07-11 17:07:59 -0700

    insert and remove can be implemented multiple times before mining

--- misc/rc.js
+++ misc/rc.js
@@ -1,3 +1,7 @@
+var shead = "";
+var stail = "";
+var size = 0;
+var sortedmap = [];
 
 function listAccounts() 
 {
@@ -73,27 +77,39 @@ function unlock()
 }
 
 function testPopulate() {
-  DLL.insert("1", "1", "1", false, {from:eth.accounts[0], gas:1000000});
-  DLL.insert("2", "2", "2", false, {from:eth.accounts[0], gas:1000000});
-  DLL.insert("3", "3", "3", false, {from:eth.accounts[0], gas:1000000});
+  DLL.insert("1", "1", "", false, {from:eth.accounts[0], gas:1000000});
+  DLL.insert("2", "2", "1", false, {from:eth.accounts[0], gas:1000000});
+  DLL.insert("3", "3", "2", false, {from:eth.accounts[0], gas:1000000});
+  shead = "1";
+  stail = "3";
+  size = size+3;
 }
 
 function getTargetKey(key) { //linear search done outside returns index before, "" if it is at front
-  e = DLL.getSortedHead();
- 
-  if(key <= e[0]) {
+  if(size == 0) {
+    return;
+  }
+
+  e = DLL.getEntry(shead);
+  
+  if(key < shead) {
     return "";
   }
   
-  a = DLL.sorted_tail();
-  if(key >= a) {
-    return a;
+  if(key > stail) {
+    return stail;
   }
   
-  while (key >= e[0] && e[0] != DLL.sorted_tail()) {
-    e = DLL.getEntry(e[5]);
+  index = 1;
+  while(key > sortedmap[index]) {
+    index++;
   }
-  return e[4];
+  return sortedmap[index-1];
+  
+ // while (key >= e[0] && e[0] != stail) {
+ //   e = DLL.getEntry(e[5]);
+ // }
+ // return e[4];
 }
 
 function insert(key, value, update) { //find the way to do mining asynchronously
@@ -101,28 +117,72 @@ function insert(key, value, update) { //find the way to do mining asynchronously
     return 0; 
   }
   
-  if(DLL.head() == "") {
-    DLL.insert(key, value, "0", update, {from:eth.accounts[0], gas:1000000});
+  if(size == 0) {
+    shead = key;
+    stail = key;
+    DLL.insert(key, value, "", update, {from:eth.accounts[0], gas:1000000});
+    sortedmap.push(key);
+    size++;
     return 1;
   }
   
   targetkey = getTargetKey(key);
   DLL.insert(key, value, targetkey, update, {from:eth.accounts[0], gas:1000000});
+ 
+  if(key < shead) {
+    shead = key;
+    sortedmap.splice(0,0,key);
+  }
+  
+  else if(key > stail) {
+    stail = key;
+    sortedmap.push(key);
+  }
+  
+  else{
+    index = 1;
+    while(key > sortedmap[index]) {
+      index++;
+    }
+    sortedmap.splice(index,0,key);
+  }
+  
+  size++;
   return 2;
 }
 
 function remove(targetkey) { 
+
+  if(size == 1) {
+    stail = "";
+    shead = "";
+    sortedmap.pop();
+  }
+  
+  else if(targetkey == stail) {
+    i = DLL.getEntry(targetkey);
+    stail = i[4];
+    sortedmap.pop();
+  }
+    
+  else if(targetkey == shead) {
+    j = DLL.getEntry(targetkey);
+    shead = j[5];
+    sortedmap.splice(0,1);
+  }
+  
+  else {
+    index = 1;
+    while(index < sortedmap.length) {
+      if(sortedmap[index] == targetkey) {
+        sortedmap.splice(index,1);
+        break;
+      }
+      index++;
+    }
+  }
+  
+  size--;
   return DLL.remove(targetkey, {from:eth.accounts[0], gas:1000000});
 }
 
-function clear() {
-  return DLL.clear({from:eth.accounts[0], gas:1000000});
-}
-
-function pop_back() {
-  return DLL.pop_back({from:eth.accounts[0], gas:1000000});
-}
-
-function pop_front() {
-  return DLL.pop_front({from:eth.accounts[0], gas:1000000});
-}
