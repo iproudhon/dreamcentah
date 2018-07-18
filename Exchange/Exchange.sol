087693f623d2a3cc79b3f5f4051133164792ec40
Seoyoon <seoyoon.jin221@gmail.com>
2018-07-18 10:35:16 -0700

    exchange.sol update

--- Exchange/Exchange.sol
+++ Exchange/Exchange.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.9;
 
+import "DLL.sol";
+
 contract Token {
     string public name; 
     uint256 public totalSupply; 
@@ -26,23 +28,50 @@ contract Token {
     }
 }
 
-contract Exchange{ 
-    address public admin;
-    address public feeAccount; //account to receive fee 
-    uint public fee; //percentage times 
+contract account { 
+   DLL allOrders;
+   DLL openBuyOrders;
+   DLL openSellOrders;
+   DLL settledOrders;
+   DLL canceledOrders;
+
+
+
+   function sellLimitOrder(string tokenName, string price, string amount) {
+       string orderKey = keccak256(msg.sender, tokenName, price, amount); //If the orderKey is not empty, then update with higher amount 
+       while (bytes(allOrders.getEntry(orderKey)[1]) != 0 ) {
+           orderKey = keccak256(orderKey); //get a different orderKey if it already exists       }
+       }
+       price = 'price:' + price;
+       amount = 'amount:' + amount;
+
+       allSellOrders.insert(orderKey, tokenName + price + amount); 
+       openSellOrders.insert(orderKey, tokenName + price + amount);
+   }
+}
+
+contract Exchange{ //Quote Driven market as opposed to order driven market 
+    DLL gAllOrders;
+    DLL gOpenOrders;
+    DLL gSettledOrders;
+    DLL gCanceledOrders;
+
     mapping (address => mapping(address => uint)) public tokens; //token address mapped to mapping of accounts to balance 
     mapping (address => mapping (bytes32=> bool)) public orders; mapping of user accounts to mapping of order 
 
-    function deposit() payable {
-
+    function deposit() payable { //tokens[0] represents ethereum
+        if (msg.value > 0)
+            tokens[0][msg.sender] += msg.value;
     }
 
-    function withdraw(uint amount) {
-
+    function withdraw(address _to, uint amount) {
+        if (tokens[0][msg.sender] < amount) throw; 
+        tokens[0][msg.sender] -= amount;
+        //transfer to _to address 
     }
 
-    function sellLimitOrder(uint amount, address token_address) {
-
+    function sellLimitOrder(uint amount, uint price, address token_address) {
+        
     }
 
     function buyLimitOrder(uint amount, address token_address) {
@@ -64,6 +93,5 @@ contract Exchange{
     function cancelOrder() {
 
     }
-
      
 }
